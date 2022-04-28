a291c4e085434e0a32ac8d158254267221f419e7
ehrazhasan <ehraz_hasan@outlook.com>
2022-04-28 20:36:06 +0530

    Updated few annotations in instiService.java

--- Backend/AdminModule/src/main/java/com/instilunchbox/AdminModule/service/instiService.java
+++ Backend/AdminModule/src/main/java/com/instilunchbox/AdminModule/service/instiService.java
@@ -2,6 +2,7 @@ package com.instilunchbox.AdminModule.service;
 
 import com.instilunchbox.AdminModule.Repo.InstiRepo;
 import com.instilunchbox.AdminModule.model.instiUser;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -15,6 +16,7 @@ public class instiService {
 
     private final InstiRepo instiRepo;
 
+    @Autowired
     public instiService(InstiRepo instiRepo) {
         this.instiRepo = instiRepo;
     }
